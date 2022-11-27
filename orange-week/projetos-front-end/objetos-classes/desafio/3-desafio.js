d041bcf9c589fd39369a3ff24a2ecb4e670d9318
Jorge-Portela <jorge.felipe.portela@gmail.com>
2022-11-26 23:56:41 -0300

    atualizar resolucao do desafio 3 classes

--- orange-week/projetos-front-end/objetos-classes/desafio/3-desafio.js
+++ orange-week/projetos-front-end/objetos-classes/desafio/3-desafio.js
@@ -44,7 +44,7 @@ class Person {
 }
 
 // uma função é sempre fora da classe, enquanto os métodos estão inseridos dentro da classe!!!
-const jose = new Person ('José Falconi', 90, 1.75)
+const jose = new Person ('José Otavio', 90, 1.75)
 console.log(jose);
 console.log('Seu IMC é: ' + jose.calculateImc().toFixed(1));
 console.log(jose.rateItImc());
