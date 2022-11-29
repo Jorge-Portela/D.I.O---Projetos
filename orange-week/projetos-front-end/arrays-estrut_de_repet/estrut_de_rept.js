3d4b951b27a8174d4a0b63b20f08fc37e63a8365
Jorge-Portela <jorge.felipe.portela@gmail.com>
2022-11-28 23:54:43 -0300

    atualizar desafio estrutura de repetição

--- orange-week/projetos-front-end/arrays-estrut_de_repet/estrut_de_rept.js
+++ orange-week/projetos-front-end/arrays-estrut_de_repet/estrut_de_rept.js
@@ -5,7 +5,6 @@ for (let i = 0; i < player.length; i++) {
     const letter = player[i];
     console.log(letter);   
 }
-
 */
 
 /*
@@ -15,10 +14,10 @@ Exercício de exemplo: Cria uma lista vazia e insira notas de alunos, e depois f
 const grade = [];
 
 grade.push(5);
-grade.push(4);
-grade.push(6);
+grade.push(7);
+grade.push(9);
 grade.push(10);
-grade.push(7.2);
+grade.push(7.9);
 console.log(grade)
 
 let sum = 0;
