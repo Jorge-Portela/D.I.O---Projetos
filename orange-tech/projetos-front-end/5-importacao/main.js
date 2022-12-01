147cea7328b2892d38db1944e80efd42c26d4130
Jorge-Portela <jorge.felipe.portela@gmail.com>
2022-12-01 19:35:11 -0300

    ADICIONAR O DIRETÓRIO REFERENTE A MÉTODO DE IMPORTAÇÃO EM NODE

---
+++ orange-tech/projetos-front-end/5-importacao/main.js
@@ -0,0 +1,22 @@
+
+// método de fazer miutos códigos com pouca linha
+const { gets, print } = require('./funcoes-auxiliares');
+
+print(gets());
+
+/*
+const funcoes = require('./funcoes-auxiliares');
+
+console.log(funcoes.gets());
+
+
+const pessoa = {
+    nome: 'vitor'
+};
+
+// destruction object
+/*
+const { nome } = pessoa;
+
+const nome = pessoa.nome;
+*/
